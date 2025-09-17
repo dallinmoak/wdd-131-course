7d132d7d572038c926ff6909f8caac790730c098
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-09-16 20:34:19 -0600

    week 1 ponder a and b

---
+++ ponder/ponder_wk1b.js
@@ -0,0 +1,27 @@
+const PI = 3.14;
+let radius = 3;
+
+let area = PI * Math.pow(radius,2);
+console.log(area);
+
+radius = 20;
+
+let newArea = PI * Math.pow(radius, 2);
+console.log(newArea);
+
+const one = 1;
+const two = '2';
+
+console.log(two);
+console.log(one + parseInt(two));
+
+let course = "CSE 131";
+
+if (true) {
+    let student = "John";
+    console.log(course);
+    console.log(student);
+}
+
+console.log(course);
+console.log(student);
